50d5c41bb1f31b76e7ae5101160a058cba88c696
bilalasghar495 <bilalasghar586@gmail.com>
2023-07-14 13:15:49 +0500

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:5000',
+  imageUrl: 'E:Angular-Projects/Assesment/AssesmentTask/src/assets',
 };
 
 /*
